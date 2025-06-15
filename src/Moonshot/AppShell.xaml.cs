fd6d4e63e9be12aefe39d558dea5e44959f51488
Marlier Thibaud <thibaud.marlier@algosup.com>
2025-06-15 14:41:22 +0200

    Initializing code framework
    - added basic pages
    - initialized .NET MAUI
    - Added a .gitignore for file cleansing

---
+++ src/Moonshot/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Moonshot;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		RegisterRoutes();
+	}
+
+	private void RegisterRoutes()
+	{
+		// Register any additional routes here
+		Routing.RegisterRoute(nameof(Pages.RegisterPage), typeof(Pages.RegisterPage));
+		Routing.RegisterRoute(nameof(Pages.LoginPage), typeof(Pages.LoginPage));
+		Routing.RegisterRoute(nameof(Pages.MenuPage), typeof(Pages.MenuPage));
+    }
+}
---
+++ src/Moonshot/Pages/MenuPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Moonshot.Pages
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
